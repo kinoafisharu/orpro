7a17550b00f37cae079d95ff7f0fb728e2e6a935
Евгений Зайцев <zaytsev.eug@gmail.com>
2018-07-16 14:04:22 +0400

    Fix priority

--- pages/views.py
+++ pages/views.py
@@ -505,7 +505,8 @@ def catalog(request, cat_url='nothing'):
 
     elif sort_by == 'priority':
         offers = offers\
-            .annotate(priority=models.Sum((F('offer_tag__tag_priority'))+F('offer_tag__tag_priority'))/(models.Count('offer_subtags')+1))\
+            .annotate(priority=models.Sum('offer_subtags__tag_priority')
+                               /(models.Count('offer_subtags') or 0))\
             .order_by('priority')
 
     elif sort_by == 'price':
