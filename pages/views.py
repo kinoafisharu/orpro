95b7a1f2e9694835e6a6549cd8b720c701d9e739
ООО "Ивановы и компания <32426965+kinoafisharu@users.noreply.github.com>
2018-07-18 10:52:08 +0300

    4

--- pages/views.py
+++ pages/views.py
@@ -506,7 +506,7 @@ def catalog(request, cat_url='nothing'):
     elif sort_by == 'priority':
 #        offers = offers.annotate(priority=models.Sum('offer_subtags__tag_priority')(models.Count('offer_subtags') or 0)).order_by('priority')
 # Изменение Иванова М. 
-        offers = sorted(Offers.offer_subtags.aggregate(Avg('priority')))
+        offers = sorted(Offers.objects.aggregate(Avg('offer_subtags.priority')))
 
     elif sort_by == 'price':
         offers = offers.extra(select={
