6aa56b14ac6535fc8e30b67017fa7757ba20fdf5
Stepan Kholodov <holodovsya@gmail.com>
2018-08-23 16:21:26 +0300

    full description bug fixed

--- pages/templatetags/get_html_comments.py
+++ pages/templatetags/get_html_comments.py
@@ -5,4 +5,4 @@ comment_search_expr = re.compile('\<![ \r\n\t]*(--([^\-]|[\r\n]|-[^\-])*--[ \r\n
 
 def get_html_comments(html, expr):
     '''Returns html comments using as space-joined string'''
-    return ' '.join([x[0][2:-2] for x in expr.findall(html)]).strip() + '...'
+    return ' '.join([x[0][2:-2] for x in expr.findall(html)]).strip()
