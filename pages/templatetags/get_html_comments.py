53ec48548a48c05b118ad534e718677b3f86d065
Ивановы и Ко <32426965+kinoafisharu@users.noreply.github.com>
2018-08-24 10:25:06 +0300

    добавил назначение функции

--- pages/templatetags/get_html_comments.py
+++ pages/templatetags/get_html_comments.py
@@ -4,5 +4,8 @@ comment_search_expr = re.compile('\<![ \r\n\t]*(--([^\-]|[\r\n]|-[^\-])*--[ \r\n
 
 
 def get_html_comments(html, expr):
-    '''Returns html comments using as space-joined string'''
+    '''Returns html comments using as space-joined string
+    Назначение: выделение в тексте полного описания товара строки для краткого описания на страницу списка товаров.
+    Теги для выделения строки: <!-- выделяемый текст -->
+    '''
     return ' '.join([x[0][2:-2] for x in expr.findall(html)]).strip()
