61cc24e2085dec0596f9a3ab9da2c517b9ec0d78
Ивановы и Ко <32426965+kinoafisharu@users.noreply.github.com>
2019-02-27 08:20:53 +0000

    удалил препенд ввв

--- app/settings.py
+++ app/settings.py
@@ -201,8 +201,6 @@ CSRF_COOKIE_SECURE = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-PREPEND_WWW = True
-
 BASE_URL = "https://orpro.ru"
 
 ALLOWED_HOSTS = ['www.orpro.ru', 'orpro.ru']
